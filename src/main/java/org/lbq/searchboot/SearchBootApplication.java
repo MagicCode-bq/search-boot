149516ffe2345840d08b924bd89d8170c22bece5
bingqing <isbingqing@163.com>
2021-04-16 14:39:45 +0800

    '初始化项目'

---
+++ src/main/java/org/lbq/searchboot/SearchBootApplication.java
@@ -0,0 +1,13 @@
+package org.lbq.searchboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lbq/searchboot/SearchBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lbq.searchboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
